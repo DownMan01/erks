cbe8e2ce41540556f9ab606da0fbf32fa1954af0
J <98541904+DownMan01@users.noreply.github.com>
2025-02-14 21:08:12 +0800

    Update script.js

--- valentines/script.js
+++ valentines/script.js
@@ -1,9 +1,3 @@
-function growRose() {
-    document.getElementById("flower").classList.add("grow");
-    document.getElementById("flower").classList.remove("hidden");
-    document.getElementById("flowerContainer").innerHTML += `
-        <svg id="flower-frame" version="1.1"
-            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 641.5 637.3">
-            <!-- Paste the SVG content from your file here -->
-        </svg>`;
+function growFlowers() {
+    document.querySelector(".flowers").classList.add("grow");
 }
